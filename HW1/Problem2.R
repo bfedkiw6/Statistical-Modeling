ea8cb61f812e13e897785d9e6eb1e932a98462e1
Ethan Wang <ethanbwang@gmail.com>
2023-04-19 10:36:40 -0700

    Fixed work in ExactAnalysis and created 2 value vector in new function

--- HW1/Problem2.R
+++ HW1/Problem2.R
@@ -10,16 +10,6 @@ ExactAnalysis <- function() {
     k <- 1
     r <- 3
     q <- 1
-    # W is how long the passenger needs to wait at the stop for a bus to arrive
-    # Bus departures at X_i
-    # L_d is the time between departures of any two buses (independent of each other)
-    # P(L_d = j) = p_j (probability that the time between departures is a certain amount)
-    # P(L_d = 1) = 0.5
-    # P(L_d = 2) = 0.5
-
-    # First bus leaves at time 0, takes 2 units to get to station
-    # Second bus has a 50-50 chance of departing 1 unit or 2 units after first bus departs
-    # So on for any future buses
 
 
     # Quantities vector
@@ -31,7 +21,7 @@ ExactAnalysis <- function() {
     # = P(L_1 = 2 and L_2 = 2) + P(L_1 = 1 and L_2 = 1 and L_3 = 2)
     # = P(L_1 = 2) * P(L_2 = 2) + P(L_1 = 1) * P(L_2 = 1) * P(L_3 = 2)
 
-    q_vect[1] <- 0.5 * 0.5 + 0.5 * 0.5 * 0.5
+    q_vect[1] <- (0.5 * 0.5) + (0.5 * 0.5 * 0.5)
 
 
     # P(X_2 = r)
@@ -39,10 +29,11 @@ ExactAnalysis <- function() {
     # = P(L_1 = 1 and L_2 = 2) + P(L_1 = 2 and L_2 = 1)
     # = P(L_1 = 1) * P(L_2 = 2) + P(L_1 = 2) * P(L_2 = 1)
 
-    q_vect[2] <- 0.5 * 0.5 * 2
+    q_vect[2] <- 2 * (0.5 * 0.5)
 
 
-    # P(W = k | L_1 = q) (probability that passenger has to wait k=1 unit of time given the delay between B_0 and B_1 = q=1)
+    # P(W = k | L_1 = q) (probability that passenger has to wait k=1 unit of time given the delay between B_1 and B_2 = q)
+    # W here only has the case where L_1 = 1, L_2 = 1, and L_3 = 2 since L_1 has to be 1
     # = P(L_2 = 1 and L_3 = 2 | L_1 = 1)
     # = P(L_2 = 1 and L_3 = 2) (Since L_2 = 1 and L_3 = 2 is independent of L_1 = 1)
 
@@ -50,8 +41,8 @@ ExactAnalysis <- function() {
 
 
     # P(U = 3) (B_U is bus that passenger boards)
-    # B_2 must leave before t = 3
-    # P(L_1 = 1 and L_2 = 1); any other combination will lead to bus 2 leaving at t=3 meaning that the passenger will board bus 2
+    # B_2 must leave before t = 3 so that the passenger misses buses 1 and 2
+    # P(L_1 = 1 and L_2 = 1); any other combination will lead to bus 2 leaving at t=3 or greater meaning that the passenger will board bus 2
     # = P(L_1 = 1) * P(L_2 = 1)
 
     q_vect[4] <- 0.5 * 0.5
@@ -65,13 +56,17 @@ ExactAnalysis <- function() {
     # 0 * P(W = 0) + 1 * P(W = 1)
     # 0 * P(L1 = 1 and L2 = 2 or L1 = 2 and L2 = 1 or L1 = 1 and L2 = 1 and L3 = 1) + 1 * P(L_1 = 2 and L_2 = 2 or L_1 = 1 and L_2 = 1 and L_3 = 2)
 
-    q_vect[5] <- 0 * (0.5 * 0.5 + 0.5 * 0.5 + 0.5 * 0.5 * 0.5) + 1 * (0.5 * 0.5 * 0.5 + 0.5 * 0.5)
+    case_W0 <- 0 * (0.5 * 0.5 + 0.5 * 0.5 + 0.5 * 0.5 * 0.5)
+    case_W1 <- 1 * (0.5 * 0.5 * 0.5 + 0.5 * 0.5)
+    q_vect[5] <- case_W0 + case_W1
 
 
     # Var(W)
     # E[X^2] = 0^2 * (0.5 * 0.5 + 0.5 * 0.5 + 0.5 * 0.5 * 0.5) + 1^2 * (0.5 * 0.5 * 0.5 + 0.5 * 0.5)
 
-    q_vect[6] <- ( 0^2 * (0.5 * 0.5 + 0.5 * 0.5 + 0.5 * 0.5 * 0.5) + 1^2 * (0.5 * 0.5 * 0.5 + 0.5 * 0.5) ) - (q_vect[5])^2
+    case_W0_squared <- 0^2 * (0.5 * 0.5 + 0.5 * 0.5 + 0.5 * 0.5 * 0.5)
+    case_W1_squared <- 1^2 * (0.5 * 0.5 * 0.5 + 0.5 * 0.5)
+    q_vect[6] <- (case_W0_squared + case_W1_squared) - (q_vect[5])^2
 
 
     # E(B_U)
@@ -80,33 +75,74 @@ ExactAnalysis <- function() {
     # P(B_2) = P(L_1 = 1 and L_2 = 2 or L_1 = 2 and L_2 = 2 or L_1 = 2 and L_2 = 1)
     # P(B_3) = P(L_1 = 1 and L_2 = 1 and L_3 = 1 or L_1 = 1 and L_2 = 1 and L_3 = 2)
 
-    q_vect[7] <- 2 * (0.5 * 0.5 + 0.5 * 0.5 + 0.5 * 0.5) + 3 * (0.5 * 0.5 * 0.5 + 0.5 * 0.5 * 0.5)
+    case_BU2 <- 2 * (0.5 * 0.5 + 0.5 * 0.5 + 0.5 * 0.5)
+    case_BU3 <- 3 * (0.5 * 0.5 * 0.5 + 0.5 * 0.5 * 0.5)
+    q_vect[7] <- case_BU2 + case_BU3
 
 
-    # E(number of buses leaving the main station by time m) (that left the station by time m?)
-    # 3, 4, 5, or 6 buses can leave by t = 5
-    # P(3) = probability of buses 1 through 3 all having a delay of 2
-    # P(4) = probability of buses 1 through 3 all having a delay of 2 except for one of them having a delay of 1
-    # P(5) = probability of buses 1 through 4 all having a delay of 1 except for one of them having a delay of 2
-    # P(6) = probability of buses 1 through 5 all having a delay of 1
+    # E(number of buses leaving the main station by time m)
+    # 2, 3, 4, or 5 buses can leave by t = 5
+    # P(2) = Delays of (2, 2, 2)
+    # P(3) = 3 combinations of delays (2, 1, 2), (2, 2, 1), and (1, 2, 2) or 3 combinations of delays (1, 2, 1, 2), (2, 1, 1, 2), or (1, 1, 2, 2)
+    # P(4) = 4 combinations of delays (1, 1, 1, 2), (1, 1, 2, 1), (1, 2, 1, 1), or (2, 1, 1, 1) or delays of (1, 1, 1, 1, 2)
+    # P(5) = Delays of (1, 1, 1, 1, 1)
 
-    q_vect[8] <- 3 * (0.5 * 0.5 * 0.5) + 4 * (3 * (0.5 * 0.5 * 0.5)) + 5 * (4 * (0.5 * 0.5 * 0.5 * 0.5)) + 6 * (0.5 * 0.5 * 0.5 * 0.5 * 0.5)
+    two_buses <- 2 * (0.5 * 0.5 * 0.5)
+    three_buses <- 3 * (3 * (0.5 * 0.5 * 0.5) + 3 * (0.5 * 0.5 * 0.5 * 0.5))
+    four_buses <- 4 * (4 * (0.5 * 0.5 * 0.5 * 0.5) + (0.5 * 0.5 * 0.5 * 0.5 * 0.5))
+    five_buses <- 5 * (0.5 * 0.5 * 0.5 * 0.5 * 0.5)
+    q_vect[8] <- two_buses + three_buses + four_buses + five_buses
 
 
     # Var(number of buses leaving the main station by time m)
 
-    q_vect[9] <- ( 9 * (0.5 * 0.5 * 0.5) + 16 * (3 * (0.5 * 0.5 * 0.5)) + 25 * (4 * (0.5 * 0.5 * 0.5 * 0.5)) + 36 * (0.5 * 0.5 * 0.5 * 0.5 * 0.5) ) - q_vect[8]^2
-    q_vect[11] <- (3-q_vect[8])^2 * (0.5 * 0.5 * 0.5) + (4-q_vect[8])^2 * (3 * (0.5 * 0.5 * 0.5)) + (5-q_vect[8])^2 * (4 * (0.5 * 0.5 * 0.5 * 0.5)) + (6-q_vect[8])^2 * (0.5 * 0.5 * 0.5 * 0.5 * 0.5)
-
+    two_buses_squared <- 2^2 * (0.5 * 0.5 * 0.5)
+    three_buses_squared <- 3^2 * (3 * (0.5 * 0.5 * 0.5) + 3 * (0.5 * 0.5 * 0.5 * 0.5))
+    four_buses_squared <- 4^2 * (4 * (0.5 * 0.5 * 0.5 * 0.5) + (0.5 * 0.5 * 0.5 * 0.5 * 0.5))
+    five_buses_squared <- 5^2 * (0.5 * 0.5 * 0.5 * 0.5 * 0.5)
+    q_vect[9] <- (two_buses_squared + three_buses_squared + four_buses_squared + five_buses_squared) - q_vect[8]^2
 
 
     # E(number of buses leaving the main station by time m | W = k)
-    # If only 3 buses leave by t=5, no bus leaves at t=3 (L_1 = 2, L_2 = 2, L_3 = 2)
-    # If only 4 buses leave by t=5, 1 combo without a bus leaving at t=3 (L_1 = 2, L_2 = 2, L_3 = 1)
-    # If only 5 buses leave by t=5, 1 combo without a bus leaving at t=3 (L_1 = 1, L_2 = 1, L_3 = 2, L_4 = 1)
-    # No combo of delay times where 6 buses can leave the station by t=5 and no bus leaves at t=3 and one bus leaves at t=4
+    # (2, 2, 2), (1, 1, 2, 2), (1, 1, 2, 1), (2, 2, 1)
 
-    q_vect[10] <- 3 * (0.5 * 0.5 * 0.5) + 4 * (0.5 * 0.5 * 0.5) + 5 * (0.5 * 0.5 * 0.5 * 0.5)
+    q_vect[10] <- 2 * (0.5 * 0.5 * 0.5) + 3 * (0.5 * 0.5 * 0.5 + 0.5 * 0.5 * 0.5 * 0.5) + 4 * (0.5 * 0.5 * 0.5 * 0.5)
+
+    return(q_vect)
+}
+
+ExactAnalysis <- function() {
+
+    # Settings
+    m <- 5  # Time passenger gets to stop of interest
+    p <- c(0.5, 0.5)  # Probabilities of delay times between successive buses
+    v <- 2  # Time it takes for a bus to reach stop of interest from main station
+
+
+    # Constants in the quantities we are trying to find
+    k <- 1
+    r <- 3
+    q <- 1
+
+
+    # Quantities vector
+    q_vect <- vector(length=2)
+
+
+    # P(X_2 = r)
+    # = P(L_1 = 1 and L_2 = 2 or L_1 = 2 and L_2 = 1)
+    # = P(L_1 = 1 and L_2 = 2) + P(L_1 = 2 and L_2 = 1)
+    # = P(L_1 = 1) * P(L_2 = 2) + P(L_1 = 2) * P(L_2 = 1)
+
+    q_vect[1] <- 2 * (0.5 * 0.5)
+
+
+    # P(W = k | L_1 = q) (probability that passenger has to wait k=1 unit of time given the delay between B_1 and B_2 = q)
+    # W here only has the case where L_1 = 1, L_2 = 1, and L_3 = 2 since L_1 has to be 1
+    # = P(L_2 = 1 and L_3 = 2 | L_1 = 1)
+    # = P(L_2 = 1 and L_3 = 2) (Since L_2 = 1 and L_3 = 2 is independent of L_1 = 1)
+
+    q_vect[2] <- 0.5 * 0.5
 
     return(q_vect)
 }
@@ -117,7 +153,7 @@ print(ExactAnalysis())
 
 busSim <- function(m,p,v,k,r,q,nDays) {
 
-    q_vect <- c()
+    q_vect <- vector(length=10)
 
     w_vals <- vector(length=nDays)  # set up space for the Ws
     x2_vals <- vector(length=nDays)
