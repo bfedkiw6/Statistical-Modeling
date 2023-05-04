83ef4c32ff55dbfaebb80b0df0bffec284e95f81
Ethan Wang <ethanbwang@gmail.com>
2023-05-04 10:35:44 -0700

    Added base case comments to Problem 3

--- HW2/Problem3.R
+++ HW2/Problem3.R
@@ -1,6 +1,9 @@
 tags <- function(m, k, s) {
 
   # Probability is 0 if any of the function parameters are 0
+  # Base cases:
+  # k < 0 (sum greater than or equal to k, need to check s in this case)
+  # s = 0 (Finished drawing s tags, need to check k in this case)
   if (m < 0 || k < 0 || s < 0) return (0)
 
   return (1)
