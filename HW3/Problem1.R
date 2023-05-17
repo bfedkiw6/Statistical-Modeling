2a762cdd207a4d715cf9970192cd993feadd9de3
Priyal Patel <priyal@campus-028-162.ucdavis.edu>
2023-05-16 17:45:09 -0700

    problem 1 changes

--- HW3/Problem1.R
+++ HW3/Problem1.R
@@ -1,13 +1,38 @@
 dpark <- function(p, d, i) {
-
+  return(((1-p)^((d-i)-1)*p) + ((1-p)^((d+i)-1)*p))
 }
 
 ppark <- function(p, d, i) {
-
+  sum <- 0
+  for (j in 0:i) {
+    if (i == 0) {
+      sum <- sum + ((1-p)^((d-j)-1)*p)
+    } else {
+      sum <- sum + (((1-p)^((d-j)-1)*p) + ((1-p)^((d+j)-1)*p))
+    }
+  }
+  return(sum)
 }
 
 qpark <- function(p, d, q) {
-
+  sum <- 0
+  temp <- 0
+  j <- 0
+  while (TRUE) {
+    if (i == 0) {
+      sum <- sum + ((1-p)^((d-j)-1)*p)
+    } else if (sum > q) {
+      temp <- j-1
+      break
+    } else if (sum == q) {
+      temp <- j
+      break
+    } else {
+      sum <- sum + (((1-p)^((d-j)-1)*p) + ((1-p)^((d+j)-1)*p))
+    }
+    j <- j+1
+  }
+  return(temp)	
 }
 
 rpark <- function(n, p, d) {
