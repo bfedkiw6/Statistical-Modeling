15eaefb807c6c24e733a2428266fb32dcf463ed3
Ethan Wang <ethanbwang@gmail.com>
2023-05-16 19:30:12 -0700

    Fixed functions

--- HW3/Problem1.R
+++ HW3/Problem1.R
@@ -1,10 +1,11 @@
 dpark <- function(p, d, i) {
 
+  # d-1 fails, p successes for spot at destination
   if (i == 0) return((1-p)^(d-1) * p)
 
+  # Get probabilities for spots with distances i before and i after the destination
   return(((1-p)^((d-i)-1)*p) + ((1-p)^((d+i)-1)*p))
 }
-dpark(0.1, 11, 1)
 
 ppark <- function(p, d, i) {
   sum <- 0
@@ -16,32 +17,27 @@ ppark <- function(p, d, i) {
 
 qpark <- function(p, d, q) {
   sum <- 0
-  temp <- 0
   j <- 0
+  # Keep adding probabilities of distance j until q is reached
   while (TRUE) {
-    if (i == 0) {
-      sum <- sum + ((1-p)^((d-j)-1)*p)
-    } else if (sum > q) {
-      temp <- j-1
-      break
-    } else if (sum == q) {
-      temp <- j
-      break
+    if (j == 0) {
+      sum <- sum + ((1-p)^(d-1)*p)
     } else {
-      sum <- sum + (((1-p)^((d-j)-1)*p) + ((1-p)^((d+j)-1)*p))
+      sum <- sum + ((1-p)^(d-j-1)*p) + ((1-p)^(d+j-1)*p)
     }
-    j <- j+1
+    # See if sum of probabilities reached q
+    if (sum > q) {
+      return(j - 1)
+    } else if (sum == q) {
+      return(j)
+    }
+    j <- j + 1
   }
-  return(temp)	
 }
 
 rpark <- function(n, p, d) {
-  dist <- c()
-  spaces <- c()
-  spaces <- rgeom(n,p)
-  for (i in 1:length(spaces)) {
-    dist[i] <- abs(d - (spaces[i] + 1))
-  }
-  return(dist)
+  # Simulate finding a parking spot n times
+  spaces <- rgeom(n, p) + 1  # + 1 since rgeom gets the number of full parking spots
+  # abs(d - spaces) gets the distance from the destination to the parking spot
+  return(abs(d - spaces))
 }
-
