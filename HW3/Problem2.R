b6c42dc6496c98ae4487f9f5982c1818b779f0bb
Ethan Wang <ethanbwang@gmail.com>
2023-05-16 19:50:15 -0700

    Fixed transition matrix creation

--- HW3/Problem2.R
+++ HW3/Problem2.R
@@ -1,24 +1,24 @@
+# See section 5.4.1 and the die game for examples
 boardGame <- function() {
   p <- matrix(rep(0,64),nrow=8)
   onesixth <- 1/6
   onethirtysixth <- 1/36
+  # Create transition matrix
   for (i in 1:8) {
     for (j in 1:6) {
       k <- i+j
-      if (k > 8) 
-        k <- k - 8
+      if (k > 8) k <- k - 8
       if (k != 4) {
         p[i,k] <- p[i,k] + onesixth
       }
     }
-    if ((4-i) > 0) {
+
+    # If can reach bonus square, update the states that can be reached from the bonus square
+    if (4 - i > 0 || i - 2 >= 4) {
       p[i,1:2] <- p[i,1:2] + onethirtysixth
       p[i,5:8] <- p[i,5:8] + onethirtysixth
     }
   }
-  print(p)
 }
 
 boardGame()
-  # See section 5.4.1 and the die game for examples
-}
