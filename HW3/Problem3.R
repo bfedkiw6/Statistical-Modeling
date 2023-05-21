9f1559cfb3a258bb74b1b1c1823060e2fe36cfc7
Ethan Wang <ethanbwang@gmail.com>
2023-05-20 18:11:09 -0700

    Finished Problem 3

--- HW3/Problem3.R
+++ HW3/Problem3.R
@@ -36,12 +36,37 @@ fillTM <- function(p, q, r, w) {
   return(trans_mat)
 }
 
+findpi1 <- function(p) {
+  n <- nrow(p)
+  imp <- diag(n) - t(p)
+  imp[n,] <- rep(1,n)
+  rhs <- c(rep(0,n-1),1)
+  solve(imp,rhs)
+}
+
 callCtr <- function(p, q, r, w) {
 
   trans_mat <- fillTM(p, q, r, w)
-  print(apply(trans_mat, 1, sum))
-  print(trans_mat)
+  pi <- findpi1(trans_mat)
+  v <- rep(0, 3)
+
+  # Long run proportion of declined calls = how often in the "max state" and a new call comes in without any calls terminating
+  call_declined_probability <- (1 - p)^(r + w) * q
+  v[1] <- pi[r + w + 1] * call_declined_probability
+
+  # Average number of calls on hold calculated using the expected value of number of calls in the waiting room
+  # If no waiting room, don't append anything to waiting_room_calls for the waiting room
+  waiting_room <- NULL
+  if (w > 0) waiting_room <- c(1:w)
+  waiting_room_calls <- c(rep(0, r + 1), waiting_room)
+  v[2] <- sum(pi * waiting_room_calls)
+
+  # Average number of busy operators calculated using the expected value of busy operators
+  busy_operators <- c(0:r, rep(r, w))
+  v[3] <- sum(pi * busy_operators)
+
+  return(v)
 }
 
-callCtr(0.4, 0.3, 2, 3)
-callCtr(0.4, 0.3, 1, 0)
+#callCtr(0.4, 0.3, 2, 3)
+#callCtr(0.4, 0.3, 1, 0)
