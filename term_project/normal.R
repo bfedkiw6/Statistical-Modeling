1e28231e54a78e8a151244272bfc43a5efefd302
bfedkiw6 <briana0nicole@gmail.com>
2023-06-06 17:55:11 -0700

    Updated normal.R function

--- term_project/normal.R
+++ term_project/normal.R
@@ -2,20 +2,21 @@
 library(stats4)
 
 # Load data
-load('Data/qeML/mlb1.RData', verbose=T)
-norm_data <- mlb1$Weight
+load('Data/WAMfair/LawschoolBrief.RData', verbose=T)
+norm_data <- lawschoolbrief$LSAT
 norm_size <- length(norm_data)
+print(norm_size)
 
 # Plot the histogram + density
-png(filename='images/MLB_Weight_Density.png', bg='white')
-hist(norm_data, probability=TRUE,main='Density Estimates of MLB Weight', xlab='Credit Score', ylim=c(0, 0.020))
+png(filename='images/Lawschool_LSAT_Density.png', bg='white')
+hist(norm_data, probability=TRUE,main='Density Estimates of Law School LSAT', xlab='LSAT', ylim=c(0, 0.050))
 lines(density(norm_data), lwd=1, col='blue')
 dev.off()
 
 # Play with Bin width
-png(filename='images/MLB_Weight_Density_bw.png', bg='white')
-hist(norm_data, probability=TRUE,main='Density Estimates of MLB Weight with Bin Width of 6', xlab='Credit Score', ylim=c(0, 0.020))
-lines(density(norm_data, bw=6), lwd=1, col='blue')
+png(filename='images/Lawschool_LSAT_Density_bw.png', bg='white')
+hist(norm_data, probability=TRUE,main='Density Estimates of Law School LSAT with Bin Width of 2', xlab='LSAT', ylim=c(0, 0.050))
+lines(density(norm_data, bw=2), lwd=1, col='blue')
 dev.off()
 
 
@@ -27,14 +28,14 @@ MM_norm_sigma <- sqrt(mean((norm_data-MM_norm_mu)^2))
 cat("MM Normal Mu:", MM_norm_mu, '\n')
 cat("MM Normal Sigma:", MM_norm_sigma, '\n')
 
-png(filename='images/MLB_Weight_Density_mm.png', bg='white')
-curve(dnorm(x,MM_norm_mu,MM_norm_sigma), from=0, to=300, col='red', main='Density Estimates of MLB Weight with MM', xlab='Credit Score', ylab='Density', ylim=c(0, 0.020))
+png(filename='images/Lawschool_LSAT_Density_mm.png', bg='white')
+curve(dnorm(x,MM_norm_mu,MM_norm_sigma), from=0, to=300, col='red', main='Density Estimates of Law School LSAT with MM', xlab='LSAT', ylab='Density', ylim=c(0, 0.050))
 lines(density(norm_data), lwd=1, col='blue')
 dev.off()
 
-png(filename='images/MLB_Weight_Density_mm_bw.png', bg='white')
-curve(dnorm(x,MM_norm_mu,MM_norm_sigma), from=0, to=300, col='red', main='Density Estimates of MLB Weight with MM and BW of 6', xlab='Credit Score', ylab='Density', ylim=c(0, 0.020))
-lines(density(norm_data, bw=6), lwd=1, col='blue')
+png(filename='images/Lawschool_LSAT_Density_mmbw.png', bg='white')
+curve(dnorm(x,MM_norm_mu,MM_norm_sigma), from=0, to=300, col='red', main='Density Estimates of Law School LSAT with MM and BW of 2', xlab='LSAT', ylab='Density', ylim=c(0, 0.050))
+lines(density(norm_data, bw=2), lwd=1, col='blue')
 dev.off()
 
 
@@ -50,8 +51,14 @@ MLE_norm_mu <- coef(summary(znorm))[1]
 MLE_norm_sigma <- coef(summary(znorm))[2]
 cat("MLE Normal Mu:", MLE_norm_mu, '\n')
 cat("MLE Normal Sigma:", MLE_norm_sigma, '\n')
-png(filename='images/MLB_Weight_Density_mle.png', bg='white')
-curve(dnorm(x,MLE_norm_mu,MLE_norm_sigma), from=0, to=300, col='red', main='Density Estimates of MLB Weight with MLE', xlab='Credit Score', ylab='Density', ylim=c(0, 0.020))
+
+png(filename='images/Lawschool_LSAT_Density_mle.png', bg='white')
+curve(dnorm(x,MLE_norm_mu,MLE_norm_sigma), from=0, to=300, col='red', main='Density Estimates of MLB Weight with MLE', xlab='LSAT', ylab='Density', ylim=c(0, 0.050))
 lines(density(norm_data), lwd=1, col='blue')
+dev.off()
+
+png(filename='images/Lawschool_LSAT_Density_mlebw.png', bg='white')
+curve(dnorm(x,MLE_norm_mu,MLE_norm_sigma), from=0, to=300, col='red', main='Density Estimates of MLB Weight with MLE and BW of 2', xlab='LSAT', ylab='Density', ylim=c(0, 0.050))
+lines(density(norm_data, bw = 2), lwd=1, col='blue')
 dev.off()
 
