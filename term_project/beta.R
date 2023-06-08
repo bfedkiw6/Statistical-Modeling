4bff620094381efa32f23f4629803b93749509b0
Priyal Patel <priyal@campus-058-113.ucdavis.edu>
2023-06-08 12:50:08 -0700

    finished

--- term_project/beta.R
+++ term_project/beta.R
@@ -1,20 +1,53 @@
-load('/Users/priyal/Desktop/ecs132/ECS132-HW/term_project/Data/fairml/communities.and.crime.rda',verbose=T)
-head(communities.and.crime)
-a <- mean(communities.and.crime$PctWorkMomYoungKids)
-b <- var(communities.and.crime$PctWorkMomYoungKids)
-beta <- (3.91366/0.50121381)-3.91366 #beta = 3.894704
-x <- communities.and.crime$PctWorkMomYoungKids
-plot(density(x),col="red",xlab=NA,ylab=NA,main=NA)
-curve(dbeta(x,3.91366,beta),col="blue",add=TRUE)
-title(main="Percent of Working Moms with Young Kids vs Density",xlab="Percent of Working Moms with Young Kids",ylab="Density")
+library(stats4)
+#load data
+weather <- read.csv("/Users/priyal/Desktop/ecs132/austinweather.csv")
+weather_data <- weather$HumidityLowPercent
+weather_data <- weather_data[which(weather_data != "-")]
+beta_data <- as.numeric(weather_data)/100
+beta_size <- length(weatherdata)
 
+#print(c(max(betadata),min(betadata)))
 
-#logic
-#a <- mean(data)
-#b <- var(data)
-#print(c(a,b))
-#mean(data) = a / (a + b)
-#b = (a - mean(data)a)/mean(data)
-#b = a/mean(data) - a
-#var(data) = ab /(a+b)^2*(a+b+1)
-#var(data) = a(a/mean(data)-a)/(a+a/mean(data) - a)^2 * (a+a/mean(data)-a+1)
+# Beta Family Plots
+png(filename='images/austinweather_densityestimates.png', bg='white')
+png(filename='images/austinweatherdensity_bw0.065.png', bg='white')
+hist(beta_data, probability=TRUE, main='Density Estimates of Low Humidity Percentage', xlab='Low Humidity Percentage', ylim=c(0, 2.5),xlim=c(0,1))
+lines(density(beta_data,bw=0.065), lwd=1, col='blue')
+dev.off()
+
+#Analysis
+
+#MM
+m <- mean(beta_data)
+v <- var(beta_data)
+#solve for MM_beta_alpha
+#m = MM_beta_alpha / (MM_beta_alpha + MM_beta_beta)
+#MM_beta_beta = (MM_beta_alpha - m*MM_beta_alpha)/m
+#MM_beta_beta = MM_beta_alpha/m - MM_beta_alpha
+#v = MM_beta_alpha*MM_beta_beta /(MM_beta_alpha+MM_beta_beta)^2*(MM_beta_alpha+MM_beta_beta+1)
+#v = MM_beta_alpha(MM_beta_alpha/m - MM_beta_alpha)/(MM_beta_alpha+MM_beta_alpha/m - MM_beta_alpha)^2 * (MM_beta_alpha + MM_beta_alpha/m - MM_beta_alpha + 1)
+MM_beta_alpha <- 3.41157
+MM_beta_beta <- (MM_beta_alpha/m)-MM_beta_alpha
+
+png(filename='images/austinweather_mm.png', bg='white')
+curve(dbeta(x,MM_beta_alpha,MM_beta_beta), col='red', main='Density Estimates of Low Humidity Percentage with MM', xlab='Low Humidity Percentage alpha=3.41157 and beta=4.176611', ylab='Density')
+lines(density(beta_data,bw=0.065), col='blue')
+dev.off()
+
+#MLE
+llbeta <- function(alpha,beta) {
+  loglik <- (beta_size*log(gamma(alpha+beta))) - (beta_size*log(gamma(alpha))) - (beta_size*log(gamma(beta))) + ((alpha-1)*sum(log(beta_data))) + ((beta-1)*sum(log(1-beta_data)))
+  return (-loglik)
+}
+
+zbeta <- mle(minuslogl=llbeta, start=list(alpha=1,beta=1))
+summary(zbeta)
+MLE_beta_alpha <- coef(summary(zbeta))[1]
+MLE_beta_beta <- coef(summary(zbeta))[2]
+cat("MLE Beta alpha:", MLE_beta_alpha, '\n')
+cat("MLE Beta beta:", MLE_beta_beta, '\n')
+
+png(filename='images/austinweather_mle.png', bg='white')
+curve(dbeta(x,MLE_beta_alpha,MLE_beta_beta),col="red", main='Density Estimates of Low Humidity Percentage with MLE', xlab='Low Humidity Percentage alpha=3.447276 and beta=4.150187', ylab='Density')
+lines(density(beta_data,bw=0.065), col='blue')
+dev.off()
