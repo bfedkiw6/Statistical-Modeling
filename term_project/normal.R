4e8f02e5dcdfd34f28de871fb587eac2062db54d
bfedkiw6 <briana0nicole@gmail.com>
2023-06-09 21:18:24 -0700

    Fixed normal.R and project report

--- term_project/normal.R
+++ term_project/normal.R
@@ -14,7 +14,7 @@ dev.off()
 
 # Play with Bin width
 png(filename='images/Lawschool_LSAT_Density_bw.png', bg='white')
-hist(norm_data, probability=TRUE,main='Density Estimates of Law School LSAT with Bin Width of 2', xlab='LSAT', ylim=c(0, 0.050))
+hist(norm_data, probability=TRUE,main='Density Estimates of Law School LSAT with Bandwidth of 2', xlab='LSAT', ylim=c(0, 0.050))
 lines(density(norm_data, bw=2), lwd=1, col='blue')
 dev.off()
 
